0abff52cca535d4b785b1c61644948d8aa5f8d35
GStudenikin <bogart250595@gmail.com>
2021-03-23 21:08:03 +0700

    Added home page

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import './main.scss';
+import './components/home/home.scss'
+import './fonts/banny/banny.scss';
+import './fonts/opensans-bold/opensans-bold.scss';
+import './fonts/material-icons/material-icons.scss';
+console.log('Hello main.js!')
