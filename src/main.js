dd47ff7c199718e86bd8be14b087c294962d24a2
GStudenikin <bogart250595@gmail.com>
2021-03-24 17:29:27 +0700

    Added About us section

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import './main.scss';
+
+// sections
 import './components/home/home.scss'
+import './components/about-us/about-us.scss'
+
 import './fonts/banny/banny.scss';
 import './fonts/opensans/opensans.scss';
 import './fonts/material-icons/material-icons.scss';
