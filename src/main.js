db89ce70d8b8dea82d7e23d3146933a0b12332fb
GStudenikin <bogart250595@gmail.com>
2021-03-31 15:10:58 +0700

    Added Private-events section

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ import './components/booking/booking.scss'
 import './components/specialties/specialties.scss'
 import './components/specialties/specialties.js'
 import './components/menu/menu.scss'
+import './components/private-events/private-events.scss'
 
 import './fonts/banny/banny.scss';
 import './fonts/opensans/opensans.scss';
