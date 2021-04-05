b99356413aed76c890d5d2885a89a8af6cd6f03b
GStudenikin <bogart250595@gmail.com>
2021-04-05 17:06:11 +0700

    Removed console log

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -4,7 +4,6 @@ export default (window.$ = window.jQuery = jquery);
 
 $(document).ready(function(){
     const items = $(".home__header__menu__item");
-    console.log(items);
     $(".home__header__menu > a, .home__down-btn, .home__main__menu__button").click(function(e) {
         var number = 7;
         for(let i = 0; i < items.length; i++) {
